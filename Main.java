2c4cc60d0498c1317b2642eebd52b597e019dca5
simeand <122308541+simeand@users.noreply.github.com>
2023-08-21 12:16:29 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Numeric.checkEvenNumber(5));
+        System.out.println(Numeric.checkEvenNumber(6));
+        System.out.println(Numeric.checkOddNumber(5));
+        System.out.println(Numeric.checkOddNumber(6));
+        System.out.println(Numeric.computeSquare(3));
+        System.out.println(Numeric.computeSquare(5));
+        System.out.println(Numeric.computeCube(3));
+        System.out.println(Numeric.computeCube(5));
+
+    }
+}
+
+
+
+// TERMINAL OUTPUT : 
+//   false
+// true
+// true
+// false
+// 9
+// 25
+// 27
+// 125
