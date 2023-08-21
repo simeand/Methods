2bbc33f92f692474261d5dd06a4466e405e0f070
simeand <122308541+simeand@users.noreply.github.com>
2023-08-21 12:17:10 +0200

    Create Numeric.java

---
+++ Numeric.java
@@ -0,0 +1,20 @@
+public class Numeric {
+
+    public static boolean checkOddNumber(int number) {
+        return number % 2 != 0;
+    }
+
+    public static boolean checkEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    public static int computeSquare(int number) {
+        return number * number;
+    }
+
+    public static int computeCube(int number) {
+        return number * number * number;
+    }
+
+
+}
